80d9d845dca53bee2dcdb78aa46fce99a005720c
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-10-25 05:30:15 -0600

    feat: Addded a controller

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /MyController/
+        public string Index()
+        {
+            return "This is lazy action . . . meh . . .";
+        }
+
+        //
+        // GET: /MyController/Sparta/
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"What up {name}, NumNums is: {numTimes}");
+        }
+    }
+}
