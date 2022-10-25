84bfcfe619a6922a32fcffb7240ccf67277a3e78
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-10-25 10:12:43 -0600

    feat: Completed adding controller tutorial

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -14,9 +14,10 @@ namespace MvcMovie.Controllers
 
         //
         // GET: /MyController/Sparta/
-        public string Welcome(string name, int numTimes = 1)
+        public string Welcome(string name, int ID = 1)
         {
-            return HtmlEncoder.Default.Encode($"What up {name}, NumNums is: {numTimes}");
+            //return HtmlEncoder.Default.Encode($"What up {name}, NumNums is: {numTimes}");
+            return HtmlEncoder.Default.Encode($"Greetings mortal {name}, ID is: {ID}");
         }
     }
 }
